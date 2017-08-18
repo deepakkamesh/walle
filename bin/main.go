4877246eddee72b11268511b679d64b98353dd2e
Deepak Guruswamy <deepak.kamesh@gmail.com>
2017-08-17 18:59:42 -0700

    Added termdraw

--- bin/main.go
+++ bin/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"time"
 
+	"github.com/deepakkamesh/termdraw"
 	"github.com/deepakkamesh/walle"
 	"github.com/deepakkamesh/walle/assistant"
 	"github.com/deepakkamesh/walle/audio"
@@ -14,6 +16,7 @@ func main() {
 	secretsFile := flag.String("secrets_file", "/Users/dkg/Downloads/walle_prototype.json", "Path to secrets file")
 	assistantScope := flag.String("assistant_scope", "https://www.googleapis.com/auth/assistant-sdk-prototype", "comma seperated list of scope urls for assistant")
 	resourcesPath := flag.String("resources_path", "../resources", "Path to resources folder")
+	enEmotion := flag.Bool("en_emotion", true, "Enables WallE facial expression mode")
 
 	flag.Parse()
 
@@ -23,14 +26,31 @@ func main() {
 	// Initialize Google Assistant.
 	ai := assistant.New(aud, *secretsFile, *assistantScope)
 
+	// Enable WallE Face.
+	var td *termdraw.Term
+	if *enEmotion {
+		td = termdraw.New()
+	}
+
 	// Build config for Walle.
 	config := &walle.WallEConfig{
-		Audio:      aud,
-		GAssistant: ai,
+		Audio:        aud,
+		GAssistant:   ai,
+		Term:         td,
+		ResourcePath: *resourcesPath,
 	}
 
 	wallE := walle.New(config)
+
+	if err := wallE.Init(); err != nil {
+		glog.Fatalf("Failure %v", err)
+	}
+
 	wallE.Run()
 
+	// Needed so termbox can cleanup.
+	t := time.NewTimer(10 * time.Millisecond)
+	<-t.C
 	glog.Infof("WallE Terminated")
+	glog.Flush()
 }
--- walle.go
+++ walle.go
@@ -1,24 +1,37 @@
 package walle
 
 import (
-	"bufio"
-	"os"
+	"errors"
+	"fmt"
+	"image"
 
 	embedded "google.golang.org/genproto/googleapis/assistant/embedded/v1alpha1"
 
+	"github.com/deepakkamesh/termdraw"
 	"github.com/deepakkamesh/walle/assistant"
 	"github.com/deepakkamesh/walle/audio"
 	"github.com/golang/glog"
+	termbox "github.com/nsf/termbox-go"
+)
+
+const (
+	EMOTION_NORM byte = iota
+	EMOTION_SPEAK
 )
 
 type WallEConfig struct {
-	Audio      *audio.Audio
-	GAssistant *assistant.GAssistant
+	Audio        *audio.Audio
+	GAssistant   *assistant.GAssistant
+	Term         *termdraw.Term
+	ResourcePath string
 }
 
 type WallE struct {
 	audio      *audio.Audio          // PortAudio IO object.
 	gAssistant *assistant.GAssistant // Google Assistant object.
+	term       *termdraw.Term        // Termdraw object
+	resPath    string
+	emotions   map[byte][]image.Image
 }
 
 func New(c *WallEConfig) *WallE {
@@ -26,46 +39,85 @@ func New(c *WallEConfig) *WallE {
 	return &WallE{
 		gAssistant: c.GAssistant,
 		audio:      c.Audio,
+		term:       c.Term,
+		resPath:    c.ResourcePath,
+		emotions:   make(map[byte][]image.Image),
 	}
 }
 
-func (s *WallE) Run() {
-
+func (s *WallE) Init() error {
 	s.audio.StartPlayback()
 
+	if err := s.term.Init(); err != nil {
+		return err
+	}
 	if err := s.gAssistant.Auth(); err != nil {
-		glog.Fatalf("Failed to authenticate assistant: %v", err)
+		return err
 	}
 
+	//	 Load emotions.
+	exNorm, err := termdraw.LoadImages(s.resPath + "/walle_normal.png")
+	if err != nil {
+		return errors.New(fmt.Sprintf("Failed to load image resources: %v", err))
+	}
+	exSpeak, err := termdraw.LoadImages(s.resPath+"/walle_normal.png",
+		s.resPath+"/walle_speaking_small.png", s.resPath+"/walle_speaking_med.png",
+		s.resPath+"/walle_speaking_large.png")
+	if err != nil {
+		return errors.New(fmt.Sprintf("Failed to load image resources: %v", err))
+	}
+
+	s.emotions[EMOTION_NORM] = exNorm
+	s.emotions[EMOTION_SPEAK] = exSpeak
+	s.term.Run()
+
+	return nil
+}
+
+func (s *WallE) Run() {
 	for {
-
-		go func() {
-			st := <-s.gAssistant.StatusCh
-			if st == embedded.ConverseResponse_END_OF_UTTERANCE {
-				glog.V(2).Infof("END_UTTERNACE")
+		evt := <-s.term.EventCh
+		if evt.Type == termbox.EventKey {
+			switch {
+			case evt.Key == termbox.KeyEsc:
+				s.term.Quit()
+				return
+			case evt.Ch == 'r':
+				s.interact()
 			}
-		}()
 
-		audioOut := s.gAssistant.ConverseWithAssistant()
-
-		// Convert assistant audio to text.
-		txt, err := SpeechToText(audioOut)
-		if err != nil {
-			glog.Errorf("Failed to recognize speech: %v", err)
-			continue
 		}
-		glog.V(1).Infof("Google Assistant said: %v", txt)
-
-		// Get sentiment analysis of text.
-		score, magnitude, err := AnalyzeSentiment(txt)
-		if err != nil {
-			glog.Errorf("Failed to analyze sentiment: %v", err)
-			continue
-		}
-		glog.V(1).Infof("Sentiment Analysis - Score:%v Magnitude:%v", score, magnitude)
-
-		// Wait for enter.
-		reader := bufio.NewReader(os.Stdin)
-		reader.ReadString('\n')
 	}
+	return
+}
+func (s *WallE) interact() {
+
+	go func() {
+		st := <-s.gAssistant.StatusCh
+		if st == embedded.ConverseResponse_END_OF_UTTERANCE {
+			glog.V(2).Infof("END_UTTERNACE")
+			s.term.Animate(s.emotions[EMOTION_SPEAK], '*', 200)
+		}
+	}()
+
+	s.term.Animate(s.emotions[EMOTION_NORM], '*', 200)
+	audioOut := s.gAssistant.ConverseWithAssistant()
+	s.term.Animate(s.emotions[EMOTION_NORM], '*', 200)
+
+	// Convert assistant audio to text.
+	txt, err := SpeechToText(audioOut)
+	if err != nil {
+		glog.Errorf("Failed to recognize speech: %v", err)
+		return
+	}
+	glog.V(1).Infof("Google Assistant said: %v", txt)
+
+	// Get sentiment analysis of text.
+	score, magnitude, err := AnalyzeSentiment(txt)
+	if err != nil {
+		glog.Errorf("Failed to analyze sentiment: %v", err)
+		return
+	}
+	glog.V(1).Infof("Sentiment Analysis - Score:%v Magnitude:%v", score, magnitude)
+
 }
